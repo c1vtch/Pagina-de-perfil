3897e6729e7bb8f8e3f899f08d492cce72fac4f8
Francisco Navarrete <frncisco1243@gmail.com>
2025-08-06 19:55:14 -0400

    se puede cambair de nombre

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let userName = document.querySelector(".user-name")
+const editBtn = document.querySelector(".edit-btn")
+
+let requestNum = document.querySelector(".requests-num")
+let numOfConnections = document.querySelector(".num-of-connections")
+let confirmBtn = document.querySelectorAll(".confirm")
+let rejectBtn = document.querySelectorAll(".reject")
+/* 
+    Cuando se da click al boton de editar se muestra un prompt
+    en el que escribir el nuevo nombre luego el nombre previo 
+    se cambia por el nuevo
+*/
+const getNewName = () =>{
+    let newName = prompt("Input the new name")
+    return newName
+}
+
+editBtn.addEventListener("click", () => {
+    let newUserName = getNewName()
+    userName.innerText = newUserName
+})
+
+
+
+confirmBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        console.log("clicked")
+       
+    })
+})
