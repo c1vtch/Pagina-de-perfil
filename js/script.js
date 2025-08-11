cdfd1395dd0ac990019c6aa3c918888497d2c57c
Francisco Navarrete <frncisco1243@gmail.com>
2025-08-10 20:08:28 -0400

    works

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let userName = document.querySelector(".user-name")
 const editBtn = document.querySelector(".edit-btn")
 
-let requestNum = document.querySelector(".requests-num")
+
+
 let numOfConnections = document.querySelector(".num-of-connections")
 let confirmBtn = document.querySelectorAll(".confirm")
 let rejectBtn = document.querySelectorAll(".reject")
@@ -20,11 +21,32 @@ editBtn.addEventListener("click", () => {
     userName.innerText = newUserName
 })
 
+const removeDiv = () =>{
+    
+}
 
-
+// Remueve el div de la solicitud
+let currentRequests = 2;
+let addedConnections = 0;
 confirmBtn.forEach((btn) => {
     let userDiv = btn.closest(".user")
-    btn.addEventListener("click", () => {
+    let userRequestDiv = userDiv.closest(".connection-request")
+    let requestsTitle = userRequestDiv.querySelector("h2")
+
+    btn.addEventListener("click", () =>{
+        userDiv.remove()
+        currentRequests -= 1
+        addedConnections += 1
+        requestsTitle.innerText = `Solicitudes de Conexion(${currentRequests})`
+        numOfConnections.innerText = `${addedConnections}`
+    })
+})
+
+
+// Remueve el div si se rechaza
+rejectBtn.forEach((btn) => {
+    let userDiv = btn.closest(".user")
+    btn.addEventListener("click", () =>{
         userDiv.remove()
     })
 })
